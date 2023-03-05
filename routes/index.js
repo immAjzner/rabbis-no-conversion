c06c8af82d5c57e1dc6e23428785de5bfb6b5c0f
immAjzner <i171088@gmail.com>
2023-03-05 22:13:23 +0200

    split EN and HE to seperate routes and files

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const isEnglish = req.query.lang === 'en';
+  setLocals(res, req, false);
+  res.render('index-he',  { title: 'הצהרה רבנית' });
+
+});
+
+/* GET home page. */
+router.get('/en', function(req, res, next) {
+  setLocals(res, req, true);
+  res.render('index-en',  { title: 'Rabbinic statement' });
+});
+
+const setLocals = (res, req, isEnglish) => {
   res.locals.isEnglish = isEnglish;
   res.locals.isMobile = req.header('user-agent').includes('Mobile');
-  res.render('index',  { title: isEnglish ? 'Rabbinic statement' : 'הצהרה רבנית' });
-});
+};
 
 module.exports = router;
